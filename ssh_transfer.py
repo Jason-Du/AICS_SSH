64688988eb856a33dee79e5fccf8c7b645d42df1
ncku_desktop <dushiun@example.com>
2021-02-19 12:50:14 +0800

    "1250 1219"

--- ssh_transfer.py
+++ ssh_transfer.py
@@ -5,6 +5,7 @@ password = "raspberry"
 hostname = "140.116.164.33"
 port =2225
 
+# COMMAMD EXE
 try:
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -17,17 +18,28 @@ try:
     # stdin, stdout, stderr = client.exec_command('ls -al')
     result = stdout.readlines()
     print (result)
-    print(os.path.dirname(__file__))
 
 except Exception:
     print ('Exception!!')
     raise
+#SSH UPLOAD
 try:
     t2 = paramiko.Transport((hostname, 2225))
     t2.connect(username=username, password=password)
     sftp = paramiko.SFTPClient.from_transport(t)
-    # sftp.put(localpath='sentfromdesktop.txt', remotepath=os.path.join(os.path.dirname(__file__),"\sentfromdesktop.txt"))
-    sftp.get(localpath='sentfromdesktop.txt', remotepath=os.path.join(os.path.dirname(__file__),"\sentfromdesktop.txt"))
+    LOCALPATH=os.path.join(os.path.dirname(__file__),"Desktop_Message.log")
+    sftp.put(localpath=LOCALPATH, remotepath="Desktop_Message.log")
+    # sftp.get(localpath="RASPBERRARY_MSEEAGE.txt", remotepath=os.path.join(os.path.dirname(__file__),"\RASPBERRARY_MSEEAGE.txt"))
+    t2.close()
+except Exception as e:
+    print (e)
+    raise
+# SSH DOWNLOAD
+try:
+    t2 = paramiko.Transport((hostname, 2225))
+    t2.connect(username=username, password=password)
+    sftp = paramiko.SFTPClient.from_transport(t)
+    sftp.get(localpath=os.path.join(os.path.dirname(__file__),"RASPBERRARY_MSEEAGE.log"), remotepath="RASPBERRARY_MSEEAGE.log")
     t2.close()
 except Exception as e:
     print (e)
